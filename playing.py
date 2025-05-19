e0ef96f9bdb6f76d2f42c4fe63af204772fbffd9
Philip N. Brown <philipnbrown@gmail.com>
2025-05-19 12:24:52 -0600

    q-learning seems to work

--- playing.py
+++ playing.py
@@ -12,14 +12,14 @@ from stable_baselines3 import A2C
 env = gym.make("CartPole-v1", render_mode="rgb_array")
 
 model = A2C("MlpPolicy", env, verbose=1,device='cpu')
-model.learn(total_timesteps=10_000)
+model.learn(total_timesteps=50_000)
 
 vec_env = model.get_env()
 obs = vec_env.reset()
-for i in range(1000):
+for i in range(10000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
     vec_env.render("human")
-    # VecEnv resets automatically
-    # if done:
-    #   obs = vec_env.reset()
+#     # VecEnv resets automatically
+#     # if done:
+#     #   obs = vec_env.reset()
